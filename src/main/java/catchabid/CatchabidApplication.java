5d547f4004c9e247ca031e80b7aa13b5785b853c
Ammar Voloder <e1527915@student.tuwien.ac.at>
2020-11-08 14:25:01 +0100

    Initial project setup

---
+++ src/main/java/catchabid/CatchabidApplication.java
@@ -0,0 +1,13 @@
+package catchabid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchabidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchabidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catchabid/CatchabidApplicationTests.java
@@ -0,0 +1,13 @@
+package catchabid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchabidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
