e14bcfdc47d3b238d19c251e0eb97b5b0f15c5d5
Bojana Kecman <e1527932@student.tuwien.ac.at>
2020-11-21 17:00:09 +0100

    added toggle password

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -10,7 +10,7 @@ import { Address } from 'src/app/models/address';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit  {
 
   model: any = {};
   address: Address = new Address();
@@ -18,6 +18,49 @@ export class RegisterComponent {
   constructor(
       private registerService: RegisterService,
       private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+    const togglePassword1 = document.querySelector('#togglePassword1');
+    const toggleConfirmPassword1 = document.querySelector('#toggleConfirmPassword1');
+    const password1 = document.querySelector('#password1');
+    const confirmPassword1 = document.querySelector('#confirmPassword1');
+    const togglePassword2 = document.querySelector('#togglePassword2');
+    const toggleConfirmPassword2 = document.querySelector('#toggleConfirmPassword2');
+    const password2 = document.querySelector('#password2');
+    const confirmPassword2 = document.querySelector('#confirmPassword2');
+
+    togglePassword1.addEventListener('click', function (e) {
+      // toggle the type attribute
+      const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+      password1.setAttribute('type', type);
+      // toggle the eye slash icon
+      this.classList.toggle('fa-eye-slash');
+    });
+
+    toggleConfirmPassword1.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = confirmPassword1.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPassword1.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('fa-eye-slash');
+    });
+
+    togglePassword2.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+        password2.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('fa-eye-slash');
+      });
+  
+      toggleConfirmPassword2.addEventListener('click', function (e) {
+          // toggle the type attribute
+          const type = confirmPassword2.getAttribute('type') === 'password' ? 'text' : 'password';
+          confirmPassword2.setAttribute('type', type);
+          // toggle the eye slash icon
+          this.classList.toggle('fa-eye-slash');
+      });
+  }
 
   openLoginModal() {
       this.modalService.dismissAll();
--- frontend/src/app/components/signin/reset-password/reset-password.component.ts
+++ frontend/src/app/components/signin/reset-password/reset-password.component.ts
@@ -19,6 +19,26 @@ export class ResetPasswordComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    const togglePassword = document.querySelector('#togglePassword');
+    const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+    const password = document.querySelector('#password');
+    const confirmPassword = document.querySelector('#confirmPassword');
+
+    togglePassword.addEventListener('click', function (e) {
+      // toggle the type attribute
+      const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+      // toggle the eye slash icon
+      this.classList.toggle('fa-eye-slash');
+    });
+
+    toggleConfirmPassword.addEventListener('click', function (e) {
+      // toggle the type attribute
+      const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+      confirmPassword.setAttribute('type', type);
+      // toggle the eye slash icon
+      this.classList.toggle('fa-eye-slash');
+  });
   }
 
   resetPassword(){
