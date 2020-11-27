32c16d374aaffed01b625a9ff624174bf96dddf4
Marija Mihajlova <e01528582@student.tuwien.ac.at>
2020-11-27 16:54:44 +0100

    fix logging, fix block overflow on frontend

--- backend/src/main/java/at/ac/ase/controllers/AuctionController.java
+++ backend/src/main/java/at/ac/ase/controllers/AuctionController.java
@@ -9,42 +9,53 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/auctions")
-@CrossOrigin
 public class AuctionController {
     private static final Logger logger = LoggerFactory.getLogger(AuctionController.class);
-
     @Autowired
     private AuctionService auctionService;
 
     @GetMapping("upcoming")
+    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     public @ResponseBody
     ResponseEntity<List<AuctionPostDTO>> getUpcomingAuctions(
             @RequestParam(required = false) Integer pageSize,
             @RequestParam(required = false) Integer pageNr) {
 
-        logger.info("Page requested:" + pageSize + " " + pageNr);
-        List<AuctionPostDTO> posts = auctionService.getUpcomingAuctions(pageSize, pageNr);
-        logger.info("Size of payload for upcoming auctions" + posts.size());
-        return new ResponseEntity<>(posts, HttpStatus.OK);
+        logger.info("Upcoming auctions requested for page size "+pageSize+" and age number requested " + pageNr);
+        try {
+            List<AuctionPostDTO> posts = auctionService.getUpcomingAuctions(pageSize, pageNr);
+            logger.info("Size of payload for upcoming auctions:" + posts.size());
+            logger.debug("Upcoming auctions sent to frontend for pageNr " + posts.size() +" : "+ posts);
+            return new ResponseEntity<>(posts, HttpStatus.OK);
+        }catch(Exception e){
+
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.resolve(500));
     }
 
     @GetMapping("all")
+    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     public @ResponseBody
     ResponseEntity<List<AuctionPostDTO>> getAllAuctions(@RequestParam(required = false) Integer pageSize,
                                                         @RequestParam(required = false) Integer pageNr) {
+        logger.info("All auctions requested for page size "+pageSize +" and age number requested" + pageNr);
         List<AuctionPostDTO> posts = auctionService.getUpcomingAuctions(pageSize,pageNr);
+        logger.info("Size of payload for all auctions: " + posts.size());
+        logger.debug("All auctions sent to frontend for pageNr "+posts.size()+" : "+ posts);
         return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
     @GetMapping("recent")
+    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     public List<AuctionPostDTO> getRecentAuctions(
             @RequestParam(required = false) Integer pageNumber,
             @RequestParam(required = false) Integer auctionsPerPage) {
-
+        logger.info("Recent auctions requested for page size "+auctionsPerPage+"and age number requested " + pageNumber);
         return auctionService.getRecentAuctions(pageNumber, auctionsPerPage);
     }
 }
