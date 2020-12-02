64ba7e25b4c1c7d59157216e39df047b651dc2f5
applerth <appler.th@gmail.com>
2020-12-02 17:58:14 +0100

    Selenium setup: dependencies added, BaseE2E added, initial page objects added

---
+++ backend/src/test/java/at/ac/ase/e2e/pages/AuctionsListArea.java
@@ -0,0 +1,54 @@
+package at.ac.ase.e2e.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class AuctionsListArea extends PageObject{
+
+    @FindBy(xpath = "(//*[contains(text(), 'Load More')])[1]")
+    private WebElement loadMoreRecentAuctionsButton;
+
+    @FindBy(xpath = "(//*[contains(text(), 'Load More')])[2]")
+    private WebElement loadMoreUpcomingAuctionsButton;
+
+    public AuctionsListArea(WebDriver driver) {
+        super(driver);
+    }
+
+    public List<WebElement> getRecentAuctions() {
+        return getDriver().findElements(By.className("recent-auction"));
+    }
+
+    public String getCreatorText(WebElement auction) {
+        return auction.findElement(By.xpath("(article/div[@class='info']/p)[1]")).getText();
+    }
+
+    public String getAuctionNameText(WebElement auction) {
+        return auction.findElement(By.xpath("(article/div[@class='info']/p)[2]")).getText();
+    }
+
+    public String getPriceText(WebElement auction) {
+        return auction.findElement(By.xpath("(article/div[@class='info']/p)[4]")).getText();
+    }
+
+    public String getCountdownTest(WebElement auction) {
+        return auction.findElement(By.xpath("(article/div[@class='info']/div/div/app-auction-countdown)")).getText();
+    }
+
+    public void clickLoadMoreRecentAuctions() {
+        loadMoreRecentAuctionsButton.click();
+    }
+
+    public void waitUntilRecentAuctionsHasMoreThan(int n) {
+        waitUntil(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("recent-auction"), n));
+    }
+
+    public void clickLoadMoreUpcomingAuctions() {
+        loadMoreUpcomingAuctionsButton.click();
+    }
+}
