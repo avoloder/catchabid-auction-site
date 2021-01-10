fbbb9fd0246f0a2326c8d2945aca1484e3132235
Bojana Kecman <e1527932@student.tuwien.ac.at>
2021-01-10 17:51:33 +0100

    additional commit

---
+++ backend/target/test-classes/testdata/clean-db.sql
@@ -0,0 +1,13 @@
+
+DELETE FROM notification;
+DELETE FROM rating;
+DELETE FROM bid;
+DELETE FROM auction_post;
+DELETE FROM auction_house;
+DELETE FROM regular_user;
+DELETE FROM auction_subscriptions;
+DELETE FROM password_reset_token;
+DELETE FROM regular_user_preferences;
+DELETE FROM location;
+delete from contact_form;
+
