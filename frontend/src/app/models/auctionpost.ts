26daf43453691c7a7e0e2fceb9e81fccae6d57c2
Ammar Voloder <e1527915@student.tuwien.ac.at>
2021-01-19 23:04:38 +0100

    commit of frontend file

--- frontend/src/app/models/auctionpost.ts
+++ frontend/src/app/models/auctionpost.ts
@@ -19,6 +19,7 @@ export class AuctionPost {
   creatorEmail: string;
   paid?: boolean;
   subscriptions: any[];
+  creatorVerified: boolean;
 
   constructor(creatorId: number, name: string, category: string, startTime: Date, endTime: Date, country: string,
               city: string, address: string, houseNr: number, minPrice: number, description: string, image: string, paid?: boolean,subscriptions?:any[]) {
