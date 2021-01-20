f14a6b9934ba146afdbfacefb6067e8cc5e2b04b
Stefan Puhalo <stefan.puhalo@witit.org>
2021-01-20 23:03:43 +0100

    sending notifications to specific user through web-socket implemented

--- backend/src/main/java/at/ac/ase/config/WebSocketConfig.java
+++ backend/src/main/java/at/ac/ase/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package at.ac.ase.config;
 
+import at.ac.ase.interceptor.NotificationUserInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,5 +25,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.setUserDestinationPrefix("/user");
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new NotificationUserInterceptor());
+    }
 
 }
--- backend/src/main/java/at/ac/ase/controllers/NotificationWebSocketController.java
+++ backend/src/main/java/at/ac/ase/controllers/NotificationWebSocketController.java
@@ -2,9 +2,11 @@ package at.ac.ase.controllers;
 
 import at.ac.ase.entities.Notification;
 import at.ac.ase.entities.User;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,14 +16,31 @@ public class NotificationWebSocketController {
     private final SimpMessagingTemplate template;
 
     @Autowired
+    private SimpUserRegistry simpUserRegistry;
+
+    @Autowired
     NotificationWebSocketController(SimpMessagingTemplate template){
         this.template = template;
     }
 
-    @Scheduled(fixedRate = 5000)
     public void sendNotification(User user, Notification notification){
-        System.out.println("SENDING");
-        template.convertAndSend("1/queue/notification", notification);
+
+        Set<SimpUser> users = simpUserRegistry.getUsers();
+
+
+        System.out.println(users.size());
+        SimpUser u1 = users.iterator().next();
+        System.out.println(u1.getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        template.convertAndSendToUser(user.getEmail(),"/queue/notification", notification);
     }
 
 }
