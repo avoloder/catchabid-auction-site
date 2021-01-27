99adbcda36bf1f449485e8c9b3e12126257381be
Ammar Voloder <e1527915@student.tuwien.ac.at>
2021-01-27 23:21:44 +0100

    Merge branch 'testing' of https://reset.inso.tuwien.ac.at/repo/2020ws-ase-pr-group/20ws_ase-pr_qse_06 into testing
    # Conflicts:
    #	backend/src/test/resources/testdata/initial-testdata.sql

--- backend/target/test-classes/testdata/clean-db.sql
+++ backend/target/test-classes/testdata/clean-db.sql
@@ -1,7 +1,9 @@
 
 DELETE FROM regular_user_preferences;
+DELETE FROM regular_user_notification;
 DELETE FROM notification;
 DELETE FROM rating;
+update auction_post set bid_id = null;
 DELETE FROM bid;
 DELETE FROM auction_subscriptions;
 DELETE FROM auction_post;
